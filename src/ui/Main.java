c3f34dcd9c0c59123cc4a6f18d87b9e2fda88b0a
Alexander <alexssjr2002@gmail.com>
2021-04-22 08:59:38 -0500

    Main class created in ui package

---
+++ src/ui/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ui;
+
+/**
+ * @author ALEX JR
+ * @author Santiago Arevalo
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
