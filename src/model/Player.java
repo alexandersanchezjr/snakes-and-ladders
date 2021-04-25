bab225ff83171c9b246accbc32e13c1452685b69
Alexander <alexssjr2002@gmail.com>
2021-04-24 20:27:31 -0500

    toSting() added

--- src/model/Player.java
+++ src/model/Player.java
@@ -4,6 +4,8 @@ public class Player {
 	
 	private char symbol;
 	private int cont;
+	private Player right;
+	
 	
 	/**
 	 * @param symbol
@@ -35,9 +37,30 @@ public class Player {
 		return cont;
 	}
 	
+	/**
+	 * @return the right
+	 */
+	public Player getRight() {
+		return right;
+	}
+
+	/**
+	 * @param right the right to set
+	 */
+	public void setRight(Player right) {
+		this.right = right;
+	}
+
 	public void increaseCont() {
 		cont++;
 	}
 	
-	
+	public String toString (Player right) {
+		String message = "";
+		if (right.getRight() == null)
+			message += right.getSymbol();
+		else 
+			message = right.getSymbol() + toString(right.getRight());
+		return message;
+	}
 }
