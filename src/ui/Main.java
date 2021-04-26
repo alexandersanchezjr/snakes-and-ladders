ee6d495e601e739080cadce13c1a8f797a79e362
santiagoarevalo <santiguitar2003@gmail.com>
2021-04-26 17:31:39 -0500

    TODO comments deleted

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -13,14 +13,12 @@ public class Main {
 	 * 
 	 */
 	public Main() {
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
