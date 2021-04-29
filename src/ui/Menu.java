e01ea60a671cc9fa646cc2db44401687956f98a1
santiagoarevalo <santiguitar2003@gmail.com>
2021-04-29 00:34:32 -0500

    playGame() method added

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -11,6 +11,10 @@ public class Menu {
 	private final static int START = 1;
 	private final static int SHOW_RANKING = 2;
 	private final static int EXIT = 3;
+	
+	private final static String MENU = "menu";
+	private final static String SIMULATION = "simul";
+	private final static String SHOW_ORIGINAL_BOARD = "num";
 
 	private Game game;
 	
@@ -44,10 +48,29 @@ public class Menu {
 		if(parts[4].contains("*") || parts[4].contains("!") || parts[4].contains("O") || parts[4].contains("X") || parts[4].contains("%") || parts[4].contains("$") || parts[4].contains("#") || parts[4].contains("+") || parts[4].contains("&")) {
 			String players = parts[4];
 			game = new Game(rows, columns, snakes, ladders, players);
-		}
-		else {
+		}else {
 			int players = Integer.parseInt(parts[4]);
 			game = new Game(rows, columns, snakes, ladders, players);
+		}
+		
+		System.out.println(game.boardToString());
+		System.out.println("Esperando salto de linea para continuar...");
+		playGame(br.readLine());
+		
+	}
+	
+	public void playGame(String input) throws IOException {
+		if(!input.equals(MENU) && !input.equals(SIMULATION) && !input.equals(SHOW_ORIGINAL_BOARD)) {
+			System.out.println(game.gameToString());
+			playGame(br.readLine());
+		}else if(input.equals(MENU)) {
+			startProgram();
+		}else if(input.equals(SIMULATION)) {
+			playGame("");
+		}else if(input.equals(SHOW_ORIGINAL_BOARD)) {
+			System.out.println(game.boardToString());
+			System.out.println("Esperando salto de linea para continuar...");
+			playGame(br.readLine());
 		}
 	}
 	
@@ -77,7 +100,6 @@ public class Menu {
 	}
 	
 	public void startProgram() throws NumberFormatException, IOException  {
-		System.out.println("¡Bienvenido a Serpientes y Escaleras!");
 		showMenu();
 		int option = readOption();
 		doOperation(option);
@@ -91,7 +113,6 @@ public class Menu {
 				option = readOption();
 			} catch (NumberFormatException e) {
 				e.printStackTrace();
-				System.out.println("Deje la bobada e imprima un numero, vuelva a primaria");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
